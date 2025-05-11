e46999daec99013b9028689f66bad810b871d834
Dqrk <staurospapadogiannopoulos@gmail.com>
2025-05-11 09:03:54 +0300

    Update tvheadend.sh

--- install/tvheadend.sh
+++ install/tvheadend.sh
@@ -1,2 +1,4 @@
-curl -1sLf 'https://dl.cloudsmith.io/public/tvheadend/tvheadend/setup.deb.sh' | sudo -E bash
+curl -fsSL https://tvheadend.org/pgp/tvheadend.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/tvheadend.gpg
+echo "deb https://apt.tvheadend.org/stable/ubuntu/ plucky main" | sudo tee /etc/apt/sources.list.d/tvheadend.list
+curl https://dqrkky.github.io/linux/update.sh | bash
 sudo apt-get install tvheadend
