009035d3ed7e7c387c340d44de10928d8dad9057
Dqrk <staurospapadogiannopoulos@gmail.com>
2025-05-11 09:14:35 +0300

    Delete install directory

--- install/tvheadend.sh
+++
@@ -1,4 +0,0 @@
-curl -fsSL https://tvheadend.org/pgp/tvheadend.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/tvheadend.gpg
-echo "deb https://apt.tvheadend.org/stable/ubuntu/ plucky main" | sudo tee /etc/apt/sources.list.d/tvheadend.list
-curl https://dqrkky.github.io/linux/update.sh | bash
-sudo apt-get install tvheadend
