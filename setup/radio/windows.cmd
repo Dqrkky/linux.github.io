e9f13ee66b3c4a018fa5f968109ff8acdccb5776
Dqrk <staurospapadogiannopoulos@gmail.com>
2025-05-22 05:45:06 +0300

    Create windows.cmd

---
+++ setup/radio/windows.cmd
@@ -0,0 +1,3 @@
+iwr https://gist.githubusercontent.com/Dqrkky/80386a79c9428ff52ad96c52fdd1b0c3/raw/9df623dd959433c781f72a2ac10f0547c9dce258/run.both -OutFile run.bat
+cmd /c run.bat
+Remove-Item run.bat
