f5d53ac701881498c8f2b821714225735adf4d73
𝒟𝓆𝓇𝓀 <staurospapadogiannopoulos@gmail.com>
2025-07-16 21:00:41 +0300

    Update getJson.sh

--- setup/ipaddress/getJson.sh
+++ setup/ipaddress/getJson.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Dangerous but does its job.
-python -c "$(curl -fsSL https://github.com/Dqrkky/linux/new/main/setup/ipaddress/getJson.py)"sh
+python -c "$(curl -fsSL https://github.com/Dqrkky/linux/setup/ipaddress/getJson.py)"
